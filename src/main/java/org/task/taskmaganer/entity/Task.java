a7c7d3cf2b631af31a2ebb09abbd32e67e47989c
Sarvar55 <sarvar.musazade@gmail.com>
2026-02-23 22:22:22 +0400

    feat(task): add Task entity and configure H2 database

---
+++ src/main/java/org/task/taskmaganer/entity/Task.java
@@ -0,0 +1,53 @@
+package org.task.taskmaganer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    private UUID id;
+    private String title;
+    private boolean done;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return done == task.done && Objects.equals(id, task.id) && Objects.equals(title, task.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, done);
+    }
+}
