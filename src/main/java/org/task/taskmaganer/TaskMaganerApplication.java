37fd524ff10bd3aef6856cb25087e25f962544af
Sarvar55 <sarvar.musazade@gmail.com>
2026-02-23 22:08:23 +0400

    feat(init): initialize Spring Boot project structure

---
+++ src/main/java/org/task/taskmaganer/TaskMaganerApplication.java
@@ -0,0 +1,13 @@
+package org.task.taskmaganer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMaganerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMaganerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/task/taskmaganer/TaskMaganerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.task.taskmaganer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMaganerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
