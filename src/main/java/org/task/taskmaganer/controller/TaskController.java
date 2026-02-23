4f625773ad692f377b83e5a4e9e02d66332fd0af
Sarvar55 <sarvar.musazade@gmail.com>
2026-02-23 23:52:08 +0400

    feat(api): implement basic CRUD endpoints in TaskController

--- src/main/java/org/task/taskmaganer/controller/TaskController.java
+++ src/main/java/org/task/taskmaganer/controller/TaskController.java
@@ -1,4 +1,37 @@
 package org.task.taskmaganer.controller;
 
+import jakarta.persistence.Entity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.task.taskmaganer.dto.request.CreateTaskRequest;
+import org.task.taskmaganer.dto.response.TaskResponse;
+import org.task.taskmaganer.service.TaskService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/tasks", name = "TaskController")
 public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<TaskResponse>> findAll() {
+        return ResponseEntity.ok(taskService.findAll());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Void> save(@RequestBody CreateTaskRequest request) {
+        taskService.save(request);
+        return ResponseEntity.status(201).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        taskService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
